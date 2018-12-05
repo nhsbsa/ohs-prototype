6d3c8ab31875953a9a4b75d9f96162240e87ad68
chmoo@NHSBSA.NHS.UK <christopher.moore6@nhs.net>
2018-12-05 16:54:59 +0000

    sorted routes and content

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res) {
 });
 
 // add your routes here
-// pair //
+// ccs no-search-results uk-pension //
 router.get(/ccs-benefit/, function (req,res){
   if(req.query.pension === "yes"){
     res.redirect('pension-date');
@@ -18,6 +18,7 @@ router.get(/ccs-benefit/, function (req,res){
   }
 });
 
+// ccs search-results uk-pension //
 router.get(/new-person-ccs-bens/, function (req,res){
   if(req.query.pension === "yes"){
     res.redirect('pension-date-2');
@@ -27,9 +28,8 @@ router.get(/new-person-ccs-bens/, function (req,res){
     res.redirect('pension-date-2');
   }
 });
-// end pair //
 
-// pair //
+// ccs no-search-results member-state-pension //
 router.get(/same-pension-form/, function (req,res){
   if(req.query.samePensionCountry === "yes"){
     res.redirect('not-entitled');
@@ -39,6 +39,7 @@ router.get(/same-pension-form/, function (req,res){
   }
 });
 
+// ccs search-results member-state-pension //
 router.get(/new-person-member-state-pension/, function (req,res){
   if(req.query.samePensionCountry === "yes"){
     res.redirect('not-entitled');
@@ -47,9 +48,17 @@ router.get(/new-person-member-state-pension/, function (req,res){
     res.redirect('another-pension-2');
   }
 });
-// end pair //
 
-// pair
+router.get(/admin-second-pension/, function (req,res){
+  if(req.query.samePensionCountry === "yes"){
+    res.redirect('not-entitled');
+  }
+  else {
+    res.redirect('another-pension-2');
+  }
+});
+
+// admin no-search-results third-country-pension //
 router.get(/another-pension-form/, function (req,res){
   if(req.query.anotherCountry === "yes") {
     res.redirect('check-your-answers');
@@ -67,7 +76,28 @@ router.get(/new-person-third-country-pension/, function (req,res){
     res.redirect('check-your-answers-2');
   }
 });
-// end pair
+
+// admin no-search-results exportable-benefits //
+router.get(/ccs-admin/, function (req,res){
+  if(req.query.pension === "pension"){
+    res.redirect('pension-date');
+  } else if(req.query.pension !== "pension") {
+    res.redirect('benefit-date');
+  } else {
+    res.redirect('benefit-date');
+  }
+});
+
+// admin search-results exportable-benefits //
+router.get(/new-person-admin/, function (req,res){
+  if(req.query.pension === "pension"){
+    res.redirect('pension-date-2');
+  } else if(req.query.pension !== "pension") {
+    res.redirect('benefit-date-2');
+  } else {
+    res.redirect('benefit-date-2');
+  }
+});
 
 router.get(/address-pension-form/, function (req,res){
   if(req.query.anotherCountry === "yes") {
@@ -105,18 +135,6 @@ router.get(/abc-2/, function (req,res){
 router.get(/create-handler/, function (req,res){
   if(req.query.radiobs === "moving"){
     res.redirect('benefit-info');
-  }
-});
-
-
-
-router.get(/ccs-admin/, function (req,res){
-  if(req.query.pension === "pension"){
-    res.redirect('pension-date');
-  } else if(req.query.pension !== "pension") {
-    res.redirect('benefit-date');
-  } else {
-    res.redirect('benefit-date');
   }
 });
 
