27bba9fb06bb6e62c2fe6ea308212d36c541b1da
Louise White <louise.white30@nhs.net>
2020-12-14 20:43:33 +0000

    new OHS prototype, duplicated original work, removed redundant folders, created v15_0 to work in

--- app/routes.js
+++ app/routes.js
@@ -13,8 +13,13 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
+// ---------------------------- //
 // Start folder specific routes
-router.use('/v15_0', require('./views/v15_0/_routes'));
+// ---------------------------- //
+
+  // v14_0 contains all work done on old OHS prototype up until December 2020
+  router.use('/v14_0', require('./views/v14_0/_routes'));
+  router.use('/v15_0', require('./views/v15_0/_routes'));
 
 // ccs no-search-results uk-pension //
 router.get(/ccs-benefit/, function (req,res){
